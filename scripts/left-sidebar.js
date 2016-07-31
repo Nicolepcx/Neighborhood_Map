3f557a41281ca59d7af69ac7f5fd3819530bbf3a
Nicolepcx <nicolekoenigstein@me.com>
2016-07-31 07:26:55 +0200

    files
    delete unused files

--- scripts/left-sidebar.js
+++
@@ -1,8 +0,0 @@
-        $(document).ready(function () {
-            $(".toggle-sidebar").click(function () {
-                $("#sidebar").toggleClass("collapsed");
-                $("#content").toggleClass("col-md-12 col-md-9");
-
-                return false;
-            });
-        });

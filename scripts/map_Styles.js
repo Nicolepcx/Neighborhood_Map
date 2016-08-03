caaf51709534058797116688ed87c221db1ce918
Nicolepcx <nicolekoenigstein@me.com>
2016-08-03 07:44:05 +0200

    Corrections
    beautify code

---
+++ scripts/map_Styles.js
@@ -0,0 +1,79 @@
+// Creates a new map an changes the styles and color
+function initMap() {
+    var styles = [
+          {
+            featureType: 'water',
+            stylers: [
+              { color: '#29b2ea' }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { color: '#ffffff' },
+              { weight: 4 }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { color: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -40 }
+            ]
+          },{
+            featureType: 'transit.station',
+            stylers: [
+              { weight: 9 },
+              { hue: '#13c7e8' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+              { visibility: 'off' }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { lightness: 100 }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { lightness: -100 }
+            ]
+          },{
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+              { visibility: 'on' },
+              { color: '#f0e4d3' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -35 }
+            ]
+          }
+        ];
+
+    var mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(37.387474, -122.057543),
+        mapTypeControl: true,
+        scrollwheel: true,
+        disableDefaultUI: false,
+        styles: styles,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: true
+    };
